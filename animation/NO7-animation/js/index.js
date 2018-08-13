0a571e0e739dd45f7cd5a1fc539853af72735494
Vanessamf <78355655cc@sina.cn>
2018-08-13 21:51:00 +0800

    修改NO7

--- animation/NO7-animation/js/index.js
+++ animation/NO7-animation/js/index.js
@@ -1,32 +1,47 @@
-var inputList=$(".form__input");
-var formList=$(".form__wrapper");
+var inputList = $(".form__input");
+var formList = $(".form__wrapper");
 
-$(document).click(function(){
+$(document).click(function() {
 	formList.children(".line").css({
-		"width":"0",
+		"width": "0",
 		"transition": "width 0.3s ease-out"
 	});
-	if($("#email").val().length>0 && $("#password").val().length>0){
+	if($("#email").val().length > 0 && $("#password").val().length > 0) {
 		$(".btn").addClass("animated pulse");
 		$(".btn").css({
-			"animation-iteration-count":"infinite"
+			"animation-iteration-count": "infinite"
 		})
 	}
 })
-inputList.click(function(event){
+inputList.click(function(event) {
 	event.stopPropagation();
 	$(".btn").removeClass("animated pulse");
 	formList.children(".line").css({
-		"width":"0",
+		"width": "0",
 		"transition": "width 0.3s ease-out"
 	});
-	$(event.target).parent().children(".line").css({
-		"width":"100%",
+	$(this).parent().children(".line").css({
+		"width": "100%",
 		"transition": ""
 	});
-	$(event.target).next().css({
-		"bottom":"10px",
-		"font-size":"14px"
-	});
-//	$(event.target).parent().css("border-bottom","2px solid #2196f3");
+	$(this).next().css({
+		"bottom": "10px",
+		"font-size": "14px"
+	})
+
+	//	$(event.target).parent().css("border-bottom","2px solid #2196f3");
+})
+
+inputList.blur(function(event) {
+	if($(this).val().length > 0) {
+		$(this).next().css({
+			"bottom": "10px",
+			"font-size": "14px"
+		})
+	} else {
+		$(this).next().css({
+			"bottom": "",
+			"font-size": ""
+		})
+	}
 })
